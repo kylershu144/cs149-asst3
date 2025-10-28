8e4eb5cc3223f619c091bf5a31037db9781241f2
kylershu144 <114771595+kylershu144@users.noreply.github.com>
2025-10-27 23:02:50 -0700

    saxpy

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -75,6 +75,9 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // https://devblogs.nvidia.com/easy-introduction-cuda-c-and-c/
     //
+    cudaMalloc(&device_x, N * sizeof(float))
+    cudaMalloc(&device_y, N * sizeof(float))
+    cudaMalloc(&device_result, N * sizeof(float))
         
     // start timing after allocation of device memory
     double startTime = CycleTimer::currentSeconds();
@@ -82,6 +85,9 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // CS149 TODO: copy input arrays to the GPU using cudaMemcpy
     //
+    cudaMemcpy(device_x, xarray, N*sizeof(float), cudaMemcpyDeviceToHost)
+    cudaMemcpy(device_y, yarray, N*sizeof(float), cudaMemcpyDeviceToHost)
+    cudaMemcpy(device_result, resultarray, N*sizeof(float), cudaMemcpyDeviceToHost)
 
    
     // run CUDA kernel. (notice the <<< >>> brackets indicating a CUDA
@@ -91,6 +97,7 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // CS149 TODO: copy result from GPU back to CPU using cudaMemcpy
     //
+    cudaMemcpy(resultarray, device_result, N*sizeof(float))
 
     
     // end timing after result has been copied back into host memory
@@ -108,6 +115,9 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // CS149 TODO: free memory buffers on the GPU using cudaFree
     //
+    cudaFree(device_x)
+    cudaFree(device_y)
+    cudaFree(device_result)
     
 }
 
